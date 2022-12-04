a4b3ab74c40ed0e231d02807fdf972e9bdc22d05
Angel de Vicente <angel.de.vicente@iac.es>
2022-12-04 13:32:33 +0000

    First commit with data

---
+++ capstone-project-notebook.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: ml_zoomcamp
+#     language: python
+#     name: ml_zoomcamp
+# ---
+
+
